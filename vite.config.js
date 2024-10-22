44fe25f1fb54b031abaad6a605f2628ab413226e
yuch3n <dustchen1126@gmail.com>
2024-10-22 16:57:21 +0800

    新增gh-page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: "/yuch3n_portfolio/",
+  plugins: [vue(), ghPages()],
 });
