a5d41fd82a4ab3eb1eddc563701b5238c78e5bf4
yuch3n <dustchen1126@gmail.com>
2024-11-13 12:03:54 +0800

    調整圖片引入方式

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { ghPages } from "vite-plugin-gh-pages";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/yuch3n_portfolio/",
   plugins: [vue(), ghPages()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
